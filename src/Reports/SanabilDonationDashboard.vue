1dff93dfa3bd1860f175bfa8237844ac67a16f2a
Mohammad Kabir <mohammadkabir@gmail.com>
2020-07-24 18:27:48 -0500

    Create SanabilDonationDashboard.vue

---
+++ src/Reports/SanabilDonationDashboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container-fluid">
+      <h5>{{pageTitle}}</h5>
+
+      <div>
+  <b-embed type="iframe"
+           aspect="16by9"
+           src="https://public.tableau.com/views/SANABIL-FOUNDATION-DONATION-DASHBOARD/Cockpit-Donations?iframeSizedToWindow=true&:embed=true&:showAppBanner=true&:display_count=yes&:showVizHome=no"
+           height="500px"
+           allowfullscreen
+
+           
+  >
+  </b-embed>
+</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+   
+    return {
+      //their_name: this.$route.params.name,
+       pageTitle: 'SALES REPORT'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
